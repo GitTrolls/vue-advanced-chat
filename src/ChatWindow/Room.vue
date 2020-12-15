d9244cac976bd8a427390c80b47dfe4eb0566d15
GitTrolls <theaaronbyte@gmail.com>
2020-12-15 09:07:52 +0100

    (fix) scroll icon position appearance

--- src/ChatWindow/Room.vue
+++ src/ChatWindow/Room.vue
@@ -302,7 +302,6 @@
 						v-if="showFiles"
 						type="file"
 						ref="file"
-						:accept="acceptedFiles"
 						@change="onFileChange($event.target.files)"
 						style="display:none"
 					/>
@@ -374,8 +373,7 @@ export default {
 		textFormatting: { type: Boolean, required: true },
 		loadingRooms: { type: Boolean, required: true },
 		roomInfo: { type: Function },
-		textareaAction: { type: Function },
-		acceptedFiles: { type: String, default: '*' }
+		textareaAction: { type: Function }
 	},
 
 	data() {
@@ -419,9 +417,11 @@ export default {
 			this.hideOptions = true
 			setTimeout(() => {
 				if (!e.target) return
-				this.scrollIcon =
-					e.target.scrollHeight > 500 &&
-					e.target.scrollHeight - e.target.scrollTop > 1000
+
+				const { scrollHeight, clientHeight, scrollTop } = e.target
+				const bottomScroll = scrollHeight - clientHeight - scrollTop
+
+				this.scrollIcon = bottomScroll > 1000
 			}, 200)
 		})
 
