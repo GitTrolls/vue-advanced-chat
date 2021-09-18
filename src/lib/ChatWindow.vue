b8a3fa8ebe15286c47a9f19c23b2531b8f6d6dba
GitTrolls <theaaronbyte@gmail.com>
2021-09-18 12:27:54 +0200

    (fix) handle multiple usertags

--- src/lib/ChatWindow.vue
+++ src/lib/ChatWindow.vue
@@ -134,7 +134,7 @@ export default {
 		textFormatting: { type: Boolean, default: true },
 		linkOptions: {
 			type: Object,
-			default: () => ({ disabled: false, target: '_blank', rel: null })
+			default: () => ({ disabled: false, target: '_blank' })
 		},
 		roomInfoEnabled: { type: Boolean, default: false },
 		textareaActionEnabled: { type: Boolean, default: false },
